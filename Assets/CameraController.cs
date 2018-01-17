92ac66a0d372e01c12d92dee0ecc3dffdeb21e09
city0409 <250924125@qq.com>
2018-01-17 16:39:36 +0800

    Initial commit

---
+++ Assets/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private Vector3 offset;
+
+    private float currentZoom;
+    [SerializeField]
+    private float minZoom = 0.5f;
+    [SerializeField]
+    private float maxZoom = 1f;
+    [Range(0f, 1f)]
+    public float speed = 0.2f;
+    [SerializeField]
+    private float angleSpeed = 40f;
+
+    private float lookAngle;
+
+    private void Update()
+    {
+        currentZoom -= Input.GetAxis("Mouse ScrollWheel") * speed;
+        currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+        lookAngle += Input.GetAxis("Horizontal") * Time.deltaTime * angleSpeed;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = target.position - offset * currentZoom;
+
+        transform.LookAt(target.position);
+
+        transform.RotateAround(target.position, Vector3.up, -lookAngle);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour 
+{
+    [SerializeField]
+    private Camera cam;
+    private NavMeshAgent agent;
+    private PlayerMotor moter;
+
+    private void Awake () 
+	{
+        agent = GetComponent<NavMeshAgent>();
+        moter = GetComponent<PlayerMotor>();
+
+    }
+
+    private void Update () 
+	{
+        if (Input .GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics . Raycast (ray ,out hit,200))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+	}
+}
