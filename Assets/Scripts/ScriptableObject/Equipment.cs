8f237bcc656773912f9242a213c451aba13923d0
city0409 <250924125@qq.com>
2018-01-23 16:28:47 +0800

    0123PM，捡起装备

---
+++ Assets/Scripts/ScriptableObject/Equipment.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "New Equipment",menuName = "Inventory/Equipment")]
+public class Equipment : Item  
+{
+    public SkinnedMeshRenderer mesh;
+
+    public int armor;
+    public int damage;
+
+	public override  void Use () 
+	{
+        base.Use();
+	}
+	
+	
+}
