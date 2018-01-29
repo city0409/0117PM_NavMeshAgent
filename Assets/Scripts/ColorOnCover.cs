f2bf682fe1d04dc3045133d65b59e2da59495e87
city0409 <250924125@qq.com>
2018-01-29 19:52:13 +0800

    0129PM，走进箱子时变色

---
+++ Assets/Scripts/ColorOnCover.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ColorOnCover : MonoBehaviour 
+{
+    [SerializeField]
+    private Color color;
+    [SerializeField]
+    private Renderer rend;
+
+    private Color[] originalColors;
+
+    private void Awake () 
+	{
+        if (rend==null)
+        {
+            rend = GetComponent<MeshRenderer>();
+        }
+
+        originalColors = rend.materials.Select(x => x.color).ToArray();
+    
+    }
+	
+    private void OnMouseEnter()
+    {
+        print("OnMouseEnter");
+        foreach (Material item in rend .materials)
+        {
+            item .color = color;
+        }
+        //rend.material .color = Color.red;
+    }
+    private void OnMouseExit()
+    {
+        print("OnMouseExit");
+        //rend.material.color = Color.green;
+        for (int i = 0; i < rend.materials.Length; i++)
+        {
+            rend.materials[i].color = originalColors[i];
+        }
+    }
+}
